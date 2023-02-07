9c41e0c57096a7aa40fbd3bdfb9f493168bd1b8d
prashubisht15 <prashantsinghbisht710@gmail.com>
2023-02-07 21:05:00 +0530

    final refactor of code

--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -13,50 +13,53 @@ const styletext = {
   transform: "translate(-50%, -50%)",
   border: "2px solid #ff8303",
   borderRadius: "4px",
-  width: "300px",
-  height: "300px",
-  background: "black"
+  width: "400px",
+  height: "400px",
+  background: "black",
 };
 
 const Tasks = ({ allEntries, setAllEntries }) => {
   const [open, setOpen] = useState(false);
-  const [title, setTitle] = useState("");
-  const [about, setAbout] = useState("");
+  const [index, setIndex] = useState(0);
   const [openInfo, setOpenInfo] = useState(false);
-  
-  const handleOpenInfo = () => setOpenInfo(true);
-  const handleCloseInfo = () => setOpenInfo(false);
 
-  const handleOpen = () => setOpen(true);
+  const handleOpenInfo = (i) => {
+    setOpenInfo(true);
+    setIndex(i);
+  };
+
+  const handleOpen = (i) => {
+    console.log("sdfgdsfg");
+    setOpen(true);
+    setIndex(i);
+  };
+
+  const handleCloseInfo = () => setOpenInfo(false);
   const handleClose = () => setOpen(false);
 
-  const handleDelete = (index) => {
+  const handleDelete = (i) => {
     const updatedentry = allEntries.filter((_, idx) => {
-      return idx !== index;
+      return idx !== i;
     });
     setAllEntries(updatedentry);
   };
 
-  const handleUpdate = (index) => {
-    allEntries.map((e, idx) => {
-      if (idx === index) {
-        e.title = title;
-        e.about = about;
-      }
-    });
-    setAllEntries(allEntries);
+  const handleUpdate = () => {
+    setIndex(-1);
     handleClose();
   };
 
   const handleTitle = (e) => {
-    setTitle(e.target.value);
+    let arr = [...allEntries];
+    arr[index].title = e.target.value;
+    setAllEntries(arr);
   };
   const handleAbout = (e) => {
-    setAbout(e.target.value);
+    let arr = [...allEntries];
+    arr[index].about = e.target.value;
+    setAllEntries(arr);
   };
-
-
-
+  console.log(allEntries, "UOO");
   return (
     <Box className="all-tasks">
       <Grid
@@ -68,66 +71,81 @@ const Tasks = ({ allEntries, setAllEntries }) => {
         columns={12}
         spacing={4}
       >
-        {(allEntries && allEntries.length)?allEntries.map((entry, index) => (
-          <Grid item xs={12} md={4} sm={4} key={index} className="grid-border">
-            <Box className="box-content">
-              <Box className="list-box">
-                <h3>{entry.title.length>20 ? entry.title.substring(0, 20) + "..." : entry.title}</h3>
-                <p>{entry.about.length>20 ? entry.about.substring(0, 20) + "..." : entry.about}</p>
-                <CloseIcon
-                  id="close"
-                  className="btn-css"
-                  onClick={() => handleDelete(index)}
-                />
+        {allEntries ? (
+          allEntries.map((entry, id) => (
+            <Grid item xs={12} md={4} sm={4} key={id} className="grid-border">
+              <Box className="box-content">
+                <Box className="list-box">
+                  <h3>
+                    {entry.title.length > 20
+                      ? entry.title.substring(0, 20) + "..."
+                      : entry.title}
+                  </h3>
+                  <p>
+                    {entry.about.length > 20
+                      ? entry.about.substring(0, 20) + "..."
+                      : entry.about}
+                  </p>
+                  <CloseIcon
+                    id="close"
+                    className="btn-css"
+                    onClick={() => handleDelete(id)}
+                  />
+                </Box>
+                <Box className="btn-display" display="flex">
+                  <InfoOutlinedIcon
+                    className="btn-css"
+                    onClick={() => handleOpenInfo(id)}
+                  />
+                  <ModeEditOutlineOutlinedIcon
+                    className="btn-css"
+                    onClick={() => {
+                      console.log("asdfsdgasdfasdfsdfasdf");
+                      handleOpen(id);
+                    }}
+                  />
+                </Box>
               </Box>
-              <Box className="btn-display" display="flex">
-                <InfoOutlinedIcon className="btn-css" onClick={handleOpenInfo}/>
-                
-                <Modal open={openInfo} onClose={handleCloseInfo}>
-                  <Box sx={styletext}>
-                    <textarea
-                      className="textarea1"
-                      value={entry.title}
-                    ></textarea>
-                    <textarea
-                      className="textarea2"
-                      value={entry.about}
-                    ></textarea>
-                  </Box>
-                </Modal>
+            </Grid>
+          ))
+        ) : (
+          <NoTask />
+        )}
 
-                <ModeEditOutlineOutlinedIcon
-                  className="btn-css"
-                  onClick={handleOpen}
-                />
-                <Modal open={open} onClose={handleClose}>
-                  <Box sx={styletext}>
-                    <textarea
-                      className="textarea1"
-                      onChange={(e) => handleTitle(e)}
-                      // value={entry.title}
-                    ></textarea>
-                    <textarea
-                      className="textarea2"
-                      onChange={(e) => handleAbout(e)}
-                      // value={entry.about}
-                    ></textarea>
-                    <button
-                      className="btn-css"
-                      onClick={() => handleUpdate(index)}
-                    >
-                      Save
-                    </button>
-                  </Box>
-                </Modal>
-              </Box>
+        {index !== -1 && (
+          <Modal open={openInfo} onClose={handleCloseInfo}>
+            <Box sx={styletext}>
+              <textarea
+                className="textarea1"
+                value={allEntries[index].title}
+              ></textarea>
+              <textarea
+                className="textarea2"
+                value={allEntries[index].about}
+              ></textarea>
             </Box>
-          </Grid>
-        )):
-      (
-     
-          <NoTask/>
-      )}
+          </Modal>
+        )}
+
+        {index !== -1 && (
+          <Modal open={open} onClose={handleClose}>
+            <Box sx={styletext}>
+              <textarea
+                className="textarea1"
+                onChange={(e) => handleTitle(e)}
+                value={allEntries[index].title}
+              ></textarea>
+              <textarea
+                className="textarea2"
+                onChange={(e) => handleAbout(e)}
+                value={allEntries[index].about}
+              ></textarea>
+              <button className="btn-css" onClick={() => handleUpdate()}>
+                Save
+              </button>
+            </Box>
+          </Modal>
+        )}
       </Grid>
     </Box>
   );
